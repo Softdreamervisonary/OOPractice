0f24378e5049417372ac1002750d755d9dae38d8
ChengHanYang <beadreamervisionary@gmail.com>
2016-02-24 21:17:45 +0800

    commit OpenClosedPrincipleSample

---
+++ OOPracticeGUI.Servcie/Open-ClosedPrinciple/OpenClosedPrincipleSample .cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPracticeGUI.Servcie
+{
+    /// <summary>
+    /// further changes of that kind are achieved by adding new code, 
+    /// not by changing old code that already works.
+    /// </summary>
+
+
+    public class OpenClosedPrincipleSample 
+    {
+        //public class Rectangle
+        //{
+        //    public double Width { get; set; }
+        //    public double Height { get; set; }
+        //}
+
+        //public class Circle
+        //{
+        //    public double Radius { get; set; }
+        //}
+
+        //public double Area(object[] shapes)
+        //{
+        //    double area = 0;
+        //    foreach (var shape in shapes)
+        //    {
+        //        if (shape is Rectangle)
+        //        {
+        //            Rectangle rectangle = (Rectangle)shape;
+        //            area += rectangle.Width * rectangle.Height;
+        //        }
+        //        else
+        //        {
+        //            Circle circle = (Circle)shape;
+        //            area += circle.Radius * circle.Radius * Math.PI;
+        //        }
+        //    }
+
+        //    return area;
+        //}
+
+
+        public abstract class Shape
+        {
+            public abstract double Area();
+        }
+
+        public class Rectangle : Shape
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public override double Area()
+            {
+                return Width * Height;
+            }
+        }
+
+        public class Circle : Shape
+        {
+            public double Radius { get; set; }
+            public override double Area()
+            {
+                return Radius * Radius * Math.PI;
+            }
+        }
+
+
+        public double Area(Shape[] shapes)
+        {
+            double area = 0;
+            foreach (var shape in shapes)
+            {
+                area += shape.Area();
+            }
+
+            return area;
+        }
+
+
+    }
+}
